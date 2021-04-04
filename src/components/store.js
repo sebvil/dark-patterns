cba60c5f35b68354428440565896cf65ef884c64
Hunter <hunterythompson@gmail.com>
2021-04-04 11:32:03 -0500

    Update to mobile store

--- src/components/store.js
+++ src/components/store.js
@@ -1,41 +1,44 @@
 import React from 'react';
-// import Header from './header.js';
+import Header from './header.js';
+import Phone from './phone.js';
 
 
 export default function Store(props) {
-    const BarStyling = {width:"20rem", background:"#F2F1F9", border:"none", padding:"0.5rem"};
+    const BarStyling = {width:"14rem", background:"#F2F1F9", border:"none", padding:"0.5rem"};
     const NavStyle = {
         nav: {
-        width: "100%",
+        width: "75%",
         float: "left",
         border: "none",
         padding: "0",
         backgroundColor: "#2F4454",
         listStyle: "none",
-        margin: "0 0 3em 0"
+        margin: "0 0 0 0",
+        overflow: "hidden"
         },
         li: {
             float: "left"
         },
         a: {
             display: "block",
-            padding: "8px 15px",
+            padding: "3px 7px",
             textDecoration: "none",
             fontWeight: "bold",
             color: "#FCB900",
-            borderRight: "1px solid #ccc"
         }
     };
     
     return (
+        <Phone>
         <div style={{background: "#2F4454"}}>
+            <Header color="#00004d"> Amacorp </Header>
             <div>
               <ul id="nav" style={NavStyle.nav}>
-                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>All</a></li>
+                {/* <li style={NavStyle.li}><a href="#" style={NavStyle.a}>All</a></li> */}
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Hole Foods</a></li>
-                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Amacorp Video</a></li>
-                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Customer Service</a></li>
-                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Pharmacy</a></li>
+                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Ama Video</a></li>
+                {/* <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Customer Service</a></li> */}
+                {/* <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Pharmacy</a></li>
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Household</a></li>
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Fashion</a></li>
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Electronics</a></li>
@@ -43,7 +46,7 @@ export default function Store(props) {
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Healthy Living</a></li>
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Pet Supplies</a></li>
                 <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Appliances</a></li>
-                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Kids & More</a></li>
+                <li style={NavStyle.li}><a href="#" style={NavStyle.a}>Kids & More</a></li> */}
               </ul>
             <input
             style={BarStyling}
@@ -56,6 +59,7 @@ export default function Store(props) {
             {props.children}
             </div>
         </div>
+        </Phone>
         
     )
 }
